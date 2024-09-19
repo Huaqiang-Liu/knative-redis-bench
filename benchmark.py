cbcce5e7e7979751783a41a938b56d7f95d5cdee
Houqian Liu <liu_houqian@stu.pku.edu.cn>
2024-09-19 12:57:01 +0000

    update test

--- benchmark.py
+++ benchmark.py
@@ -6,13 +6,11 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-
-# 一倍的负载对应1ms的任务，10ms等等以此类推
-def task(redis_host, redis_port, rate = 1):
+def unit_task(redis_host, redis_port, rate = 1):
     redis_password = 'pmT0bVLwFr' # os.getenv('REDIS_PASSWORD', None)
     r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
 
-    for i in range(rate * 10):
+    for i in range(rate):
         r.set(f'key_{i}', f'value_{i}')
         value = r.get(f'key_{i}')
         # print(f'key_{i}: {value}')
@@ -22,8 +20,8 @@ def gen_task():
     redis_host = os.getenv('REDIS_HOST', 'localhost')
     redis_port = int(os.getenv('REDIS_PORT', 6379))
 
-    job_len = [1, 3, 25, 250, 7500]
-    job_weight = [0.05, 0.03, 0.17, 0.33, 0.37]
+    job_len = [8000, 4000, 2000, 1000, 700, 500, 350, 250, 200, 150, 100, 50, 40, 30, 25, 15, 5, 3, 2, 1]
+    job_weight = [0.05] * 20
     
     # 若使用scalable.yaml部署任务，则有环境变量TASK_SIZE
     # env:
@@ -35,7 +33,7 @@ def gen_task():
     
     rate = random.choices(job_len, job_weight)[0]
     start_time = time.time() * 1000
-    task(redis_host, redis_port, rate)
+    unit_task(redis_host, redis_port, rate)
     end_time = time.time() * 1000
     
     # 返回“任务大小 开始时间戳 结束时间戳 持续时间”
