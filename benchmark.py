b2e30b245ebac6525a08343f64dc243a4d5203b7
Houqian Liu <liu_houqian@stu.pku.edu.cn>
2024-10-08 14:51:38 +0000

    完成延迟绑定的实验

--- benchmark.py
+++ benchmark.py
@@ -31,13 +31,26 @@ def gen_task():
     # job_len = [int(os.getenv('TASK_SIZE', 1))]
     # job_weight = [1]
     
+    request_route_timestamp = request.headers.get('X-Request-Timestamp')
+    if not request_route_timestamp:
+        return "No timestamp found in request headers."
+    request_route_timestamp = int(request_route_timestamp)
+    
     rate = random.choices(job_len, job_weight)[0]
     start_time = time.time() * 1000
     unit_task(redis_host, redis_port, rate)
     end_time = time.time() * 1000
     
-    # 返回“任务大小 开始时间戳 结束时间戳 持续时间”
-    ret = f'{rate} {start_time} {end_time} {end_time - start_time}\n'
+    # 返回“任务大小 开始时间戳 结束时间戳 持续时间 JCT”
+    ret = f'{rate} {start_time} {end_time} {end_time - start_time} {end_time - request_route_timestamp}\n'
+    
+    # 将ret发送给activator，在终端里向activator发包的方式：curl -X POST http://172.18.0.10:30001/ -v
+    # activator_url = 'http://172.18.0.3:30001/'
+    # try:
+    #     response = requests.post(activator_url, data=ret)
+    #     response.raise_for_status()
+    # except requests.exceptions.RequestException as e:
+    #     print(f'Error sending data to activator: {e}')
     return ret
 
 if __name__ == "__main__":
